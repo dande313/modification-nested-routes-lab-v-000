be5f6ffd2f15bf59966161b0716ac65f6d7c7b8c
Allen Anderson <dande313@byu.net>
2017-10-04 20:48:39 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,12 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(artist, song)
+    if !artist.nil?
+      artist.name
+    else
+      select_tag "song[artist_id]", options_for_select(Artist.all.collect{ |u| [u.id, u.name] })
+    end
+  end
 end
